e39ce7176d8f75da820b9dfc2b1514f1693e1aa9
ChrisMagnuson <hourglasssand@gmail.com>
2016-10-26 12:52:18 -0400

    Add RequestorFirstName and RequestorEmailAddress custom properies

--- TervisTrackITWebAPIPowerShell.psm1
+++ TervisTrackITWebAPIPowerShell.psm1
@@ -1,13 +1,32 @@
-﻿#Requires -modules TrackITWebAPIPowerShell
+﻿#Requires -modules TrackITWebAPIPowerShell, TervisActiveDirectory
 
 Function Add-TrackITWorkOrderCustomProperties {
     param(
         [Parameter(ValueFromPipeline)]$WorkOrder
     )
-    $WorkOrder | Add-Member -MemberType ScriptProperty -Name AllNotes -Value {
-        $This.notes | GM | where membertype -EQ noteproperty | % { $This.notes.$($_.name) }
+
+    $WorkOrder | 
+    Add-Member -MemberType ScriptProperty -Name RequestorFirstName -PassThru -Value {
+        $This.RequestorName -split " " |
+        select -First 1
+    } | 
+    Add-Member -MemberType ScriptProperty -Name RequestorEmailAddress -PassThru -Value {
+        Get-ADUserEmailAddressByName -Name $This.RequestorName
+    } |
+    Add-Member -MemberType ScriptProperty -Name AllNotes -Value {
+        $This.notes | 
+        Get-Member | 
+        where membertype -EQ noteproperty | 
+        ForEach-Object { 
+            $This.notes.$($_.name) 
+        }
     }
-    $WorkOrder.AllNotes | Add-Member -MemberType ScriptProperty -Name CreatedDateDate -Value { get-date $this.createddate }
+
+    $WorkOrder.AllNotes | 
+    Add-Member -MemberType ScriptProperty -Name CreatedDateDate -Value { 
+        get-date $this.createddate 
+    }
+
     $WorkOrder
 }
 
